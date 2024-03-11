4be984768be0265dc0e836795d0ea2ecc5948674
juanc004 <124308226+juanc004@users.noreply.github.com>
2024-03-11 11:08:37 -0500

    updates to css classes for responsive design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,25 +6,25 @@ export const Footer = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Reach Out @</h2>
+        <p>Social Media Platforms</p>
       </div>
       <ul className={styles.links}>
         {/* Iterate over an array of contact methods here for cleaner JSX */}
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email" />
-          <a href="mailto:cardenasjuan1213@gmail.com">Email</a>
+          <a href="mailto:cardenasjuan1213@gmail.com">@Email</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/juancardenas01/">@linkedin</a>
+          <a href="https://www.linkedin.com/in/juancardenas01/">@LinkedIn</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/juanc004">@github</a>
+          <a href="https://github.com/juanc004">@Github</a>
         </li>
       </ul>
     </footer>
